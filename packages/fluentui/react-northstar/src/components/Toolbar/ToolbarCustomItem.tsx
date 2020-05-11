f598d964c02cc7d602ec9ea826a815c1284e54b8
Oleksandr Fediashov <olfedias@microsoft.com>
2020-05-11 15:50:00 +0200

    complete Toolbar and ToolbarCustomItem

--- packages/fluentui/react-northstar/src/components/Toolbar/ToolbarCustomItem.tsx
+++ packages/fluentui/react-northstar/src/components/Toolbar/ToolbarCustomItem.tsx
@@ -1,5 +1,13 @@
 import { Accessibility, IS_FOCUSABLE_ATTRIBUTE } from '@fluentui/accessibility';
-import { getElementType, useUnhandledProps, useAccessibility, useStyles, useTelemetry } from '@fluentui/react-bindings';
+import {
+  compose,
+  getElementType,
+  useUnhandledProps,
+  useAccessibility,
+  useStyles,
+  useTelemetry,
+  ComponentWithAs,
+} from '@fluentui/react-bindings';
 import { mergeComponentVariables } from '@fluentui/styles';
 import * as _ from 'lodash';
 import * as PropTypes from 'prop-types';
@@ -7,13 +15,7 @@ import * as React from 'react';
 // @ts-ignore
 import { ThemeContext } from 'react-fela';
 
-import {
-  ComponentEventHandler,
-  FluentComponentStaticProps,
-  ProviderContextPrepared,
-  WithAsProp,
-  withSafeTypeForAs,
-} from '../../types';
+import { ComponentEventHandler, ProviderContextPrepared } from '../../types';
 import {
   ChildrenComponentProps,
   ContentComponentProps,
@@ -21,6 +23,8 @@ import {
   UIComponentProps,
   childrenExist,
   commonPropTypes,
+  ShorthandFactory,
+  ShorthandConfig,
 } from '../../utils';
 import { ToolbarVariablesContext } from './toolbarVariablesContext';
 
@@ -57,61 +61,88 @@ export interface ToolbarCustomItemProps extends UIComponentProps, ChildrenCompon
 export type ToolbarCustomItemStylesProps = Required<Pick<ToolbarCustomItemProps, 'fitted'>>;
 export const toolbarCustomItemClassName = 'ui-toolbar__customitem';
 
-const ToolbarCustomItem: React.FC<WithAsProp<ToolbarCustomItemProps>> & FluentComponentStaticProps = props => {
-  const context: ProviderContextPrepared = React.useContext(ThemeContext);
-  const { setStart, setEnd } = useTelemetry(ToolbarCustomItem.displayName, context.telemetry);
-  setStart();
+/**
+ * A ToolbarCustomItem renders Toolbar item as a non-actionable `div` with custom content inside.
+ */
+const ToolbarCustomItem = compose<'div', ToolbarCustomItemProps, ToolbarCustomItemStylesProps, {}, {}>(
+  (props, ref, composeOptions) => {
+    const context: ProviderContextPrepared = React.useContext(ThemeContext);
+    const { setStart, setEnd } = useTelemetry(composeOptions.displayName, context.telemetry);
+    setStart();
 
-  const { accessibility, children, className, content, design, fitted, focusable, styles, variables } = props;
-  const parentVariables = React.useContext(ToolbarVariablesContext);
+    const { accessibility, children, className, content, design, fitted, focusable, styles, variables } = props;
+    const parentVariables = React.useContext(ToolbarVariablesContext);
 
-  const getA11yProps = useAccessibility(accessibility, {
-    debugName: ToolbarCustomItem.displayName,
-    rtl: context.rtl,
-  });
-  const { classes } = useStyles<ToolbarCustomItemStylesProps>(ToolbarCustomItem.displayName, {
+    const getA11yProps = useAccessibility(accessibility, {
+      debugName: composeOptions.displayName,
+      rtl: context.rtl,
+    });
+    const { classes } = useStyles<ToolbarCustomItemStylesProps>(composeOptions.displayName, {
+      className: composeOptions.className,
+      mapPropsToStyles: () => ({ fitted }),
+      mapPropsToInlineStyles: () => ({
+        className,
+        design,
+        styles,
+        variables: mergeComponentVariables(parentVariables, variables),
+      }),
+      rtl: context.rtl,
+    });
+
+    const ElementType = getElementType(props);
+    const unhandledProps = useUnhandledProps(composeOptions.handledProps, props);
+
+    const handleBlur = (e: React.SyntheticEvent) => {
+      if (focusable) {
+        _.invoke(props, 'onBlur', e, props);
+      }
+    };
+
+    const handleFocus = (e: React.SyntheticEvent) => {
+      if (focusable) {
+        _.invoke(props, 'onFocus', e, props);
+      }
+    };
+
+    const element = (
+      <ElementType
+        {...getA11yProps('root', {
+          [IS_FOCUSABLE_ATTRIBUTE]: focusable,
+          ...unhandledProps,
+          className: classes.root,
+          onBlur: handleBlur,
+          onFocus: handleFocus,
+          ref,
+        })}
+      >
+        {childrenExist(children) ? children : content}
+      </ElementType>
+    );
+    setEnd();
+
+    return element;
+  },
+  {
     className: toolbarCustomItemClassName,
-    mapPropsToStyles: () => ({ fitted }),
-    mapPropsToInlineStyles: () => ({
-      className,
-      design,
-      styles,
-      variables: mergeComponentVariables(parentVariables, variables),
-    }),
-    rtl: context.rtl,
-  });
-
-  const ElementType = getElementType(props);
-  const unhandledProps = useUnhandledProps(ToolbarCustomItem.handledProps, props);
-
-  const handleBlur = (e: React.SyntheticEvent) => {
-    if (focusable) {
-      _.invoke(props, 'onBlur', e, props);
-    }
-  };
-
-  const handleFocus = (e: React.SyntheticEvent) => {
-    if (focusable) {
-      _.invoke(props, 'onFocus', e, props);
-    }
-  };
-
-  const element = (
-    <ElementType
-      {...getA11yProps('root', {
-        [IS_FOCUSABLE_ATTRIBUTE]: focusable,
-        ...unhandledProps,
-        className: classes.root,
-        onBlur: handleBlur,
-        onFocus: handleFocus,
-      })}
-    >
-      {childrenExist(children) ? children : content}
-    </ElementType>
-  );
-  setEnd();
-
-  return element;
+    displayName: 'ToolbarCustomItem',
+    handledProps: [
+      'accessibility',
+      'as',
+      'children',
+      'className',
+      'content',
+      'design',
+      'fitted',
+      'focusable',
+      'onBlur',
+      'onFocus',
+      'styles',
+      'variables',
+    ],
+  },
+) as ComponentWithAs<'div', ToolbarCustomItemProps> & {
+  create: ShorthandFactory<ToolbarCustomItemProps>;
+  shorthandConfig: ShorthandConfig<ToolbarCustomItemProps>;
 };
 
 ToolbarCustomItem.displayName = 'ToolbarCustomItem';
@@ -126,14 +157,14 @@ ToolbarCustomItem.propTypes = {
   onFocus: PropTypes.func,
   onBlur: PropTypes.func,
 };
-ToolbarCustomItem.handledProps = Object.keys(ToolbarCustomItem.propTypes) as any;
 
 ToolbarCustomItem.create = createShorthandFactory({
   Component: ToolbarCustomItem,
   mappedProp: 'content',
 });
+ToolbarCustomItem.shorthandConfig = { mappedProp: 'content' };
 
 /**
  * A ToolbarCustomItem renders Toolbar item as a non-actionable `div` with custom content inside.
  */
-export default withSafeTypeForAs<typeof ToolbarCustomItem, ToolbarCustomItemProps>(ToolbarCustomItem);
+export default ToolbarCustomItem;
